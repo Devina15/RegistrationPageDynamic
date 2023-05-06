31eab4c49301530b63f8cca54f6fd451640a3e77
Devina15 <130356637+Devina15@users.noreply.github.com>
2023-05-06 19:07:06 +0530

    reject(try-catch)

--- asyncAwait.js
+++ asyncAwait.js
@@ -4,21 +4,17 @@ console.log('person2: shows ticket');
 const preMovie=async()=> {
     const promiseWifeBringsTicks = new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        resolve('ticket');
+        reject('ticket');
         },3000);
     });
     
-    const getPopcorn = new Promise((resolve, reject)=>resolve('popcorn '));
-
-    const getCandy = new Promise((resolve,reject)=>resolve('candy '));
+    let ticket
+    try{
+        ticket = await promiseWifeBringsTicks;
+    } catch(e){
+        ticket = 'sad face' 
+    }
     
-    const getColdDrinks = new Promise((resolve,reject)=>resolve('Cold Drinks'));
-
-    let ticket = await promiseWifeBringsTicks;
-    
-    let [popcorn, candy, drink] = await Promise.all([getPopcorn,getCandy,getColdDrinks])
-    
-    console.log(popcorn+candy+drink)
     return ticket;
 };
 preMovie().then((m)=>console.log('person3: shows '+m));
